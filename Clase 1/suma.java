a8e700e491301b9525fae60b9d8c5582ade4000d
JJ343 <125293683+JJ343@users.noreply.github.com>
2023-02-25 10:49:37 -0500

    Update suma.java

--- Clase 1/suma.java
+++ Clase 1/suma.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class suma {
     public static void main(String[] args) {
         double suma, X,Y;
         suma=0;
-        X=20;
-        Y=40;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el valor de X");
+        X= entrada.nextDouble();
+        System.out.println("Ingrese el valor de Y");
+        Y=entrada.nextDouble();
         suma=suma+X;
         X= (X+Math.pow(Y,2));
         suma=suma+X/Y;
-
+        suma=Math.round(suma*100.0)/100.0;
         System.out.println("El valor de la suma es " + suma);
 
 
