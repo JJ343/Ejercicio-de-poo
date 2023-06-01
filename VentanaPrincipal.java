767dd476dab984717485bba3cad59b98b0565895
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-06-01 09:32:38 -0500

    Update VentanaPrincipal.java

--- VentanaPrincipal.java
+++ VentanaPrincipal.java
@@ -1,9 +1,10 @@
 
+package Principal;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
 
 
 public class VentanaPrincipal extends JFrame implements
@@ -23,7 +24,7 @@ public class VentanaPrincipal extends JFrame implements
         lista = new ListaPersonas();
         inicio();
         setTitle("Personas");
-        setSize(270,350);
+        setSize(350,400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
@@ -34,45 +35,45 @@ public class VentanaPrincipal extends JFrame implements
         contenedor.setLayout(null);
         nombre = new JLabel();
         nombre.setText("Nombre:");
-        nombre.setBounds(20, 20, 135, 23);
+        nombre.setBounds(20, 25, 200, 23);
         campoNombre = new JTextField();
 
-        campoNombre.setBounds(105, 20, 135, 23);
+        campoNombre.setBounds(105, 25, 200, 23);
 
         apellidos = new JLabel();
         apellidos.setText("Apellidos:");
-        apellidos.setBounds(20, 50, 135, 23);
+        apellidos.setBounds(20, 50, 200, 23);
         campoApellidos = new JTextField();
-        campoApellidos.setBounds(105, 50, 135, 23);
+        campoApellidos.setBounds(105, 50, 200, 23);
 
         teléfono = new JLabel();
         teléfono.setText("Teléfono:");
-        teléfono.setBounds(20, 80, 135, 23);
+        teléfono.setBounds(20, 80, 200, 23);
         campoTeléfono = new JTextField();
 
-        campoTeléfono.setBounds(105, 80, 135, 23);
+        campoTeléfono.setBounds(105, 80, 200, 23);
 
         dirección = new JLabel();
         dirección.setText("Dirección:");
-        dirección.setBounds(20, 110, 135, 23);
+        dirección.setBounds(20, 110, 200, 23);
         campoDirección = new JTextField();
 
-        campoDirección.setBounds(105, 110, 135, 23);
+        campoDirección.setBounds(105, 110, 200, 23);
 
         añadir = new JButton();
         añadir.setText("Añadir");
-        añadir.setBounds(105, 150, 80, 23);
+        añadir.setBounds(105, 150, 100, 23);
 
         añadir.addActionListener(this);
 
         eliminar= new JButton();
         eliminar.setText("Eliminar");
-        eliminar.setBounds(20, 280, 80, 23);
+        eliminar.setBounds(40, 280, 120, 23);
         eliminar.addActionListener(this);
 
         borrarLista= new JButton();
         borrarLista.setText("Borrar Lista");
-        borrarLista.setBounds(120, 280, 120, 23);
+        borrarLista.setBounds(170, 280, 120, 23);
 
         borrarLista.addActionListener(this);
 
@@ -83,7 +84,7 @@ public class VentanaPrincipal extends JFrame implements
 
         scrollLista = new JScrollPane();
 
-        scrollLista.setBounds(20, 190 ,220, 80);
+        scrollLista.setBounds(20, 190 ,300, 80);
 
         scrollLista.setViewportView(listaNombres);
 
