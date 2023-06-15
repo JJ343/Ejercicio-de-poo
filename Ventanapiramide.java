5dafecf74eaa3550564bec36ba8d45e01b3aef40
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-06-15 12:39:28 -0500

    Update Ventanapiramide.java

--- Ventanapiramide.java
+++ Ventanapiramide.java
@@ -20,7 +20,7 @@ public class Ventanapiramide extends  JFrame{
 
 
         calcularButton.addActionListener(new ActionListener() {
-            @Override
+            
             public void actionPerformed(ActionEvent e) {
                 Piramide piramide;
                 boolean error = false;
