081c75ad5f7b5a2b7ef03b7c2372556cd7e170bd
NicolasOrozcoMedina <125750000+NicolasOrozcoMedina@users.noreply.github.com>
2023-06-15 12:40:47 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Principal {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
+            
             public void run() {
                 JFrame Miventana =new VentanaPrincipal();
                 Miventana.setVisible(true);
